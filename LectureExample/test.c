a12fcac073f2cff16485d1ee2709456cc7aca63c
Hiroki39 <zhenghongyi2000@hotmail.com>
2020-10-19 02:55:21 -0400

    modify lab01

--- LectureExample/test.c
+++ LectureExample/test.c
@@ -2,18 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    int x = 0x80000000;
-    int z;
-    printf("%i\n", x - 1);
-    printf("%i\n", (x - 1) >> 31);
-    printf("%i\n", ((x - 1) >> 31) & 1);
-    z = !((x - 1) >> 31) & 1;
-    printf("%i\n", z);
-
-    int y = 0x80000000;
-    y = y - 1;
-    printf("%i\n", y);
-    printf("%i\n", y >> 31);
-    printf("%i\n", (y >> 31) & 1);
-    printf("%i\n", !((y >> 31) & 1));
+    int x = 0x7FFFFFFF;
+    int x_plus_one = x + 1;
+    printf("%i\n", x_plus_one + x_plus_one);
 }
